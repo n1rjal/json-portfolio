f5cfa032805cc186c854c5a5afe3e0a4cc7da241
n1rjal <nirjalpaudel54312@gmail.com>
2021-03-04 23:26:24 +0545

    transfereed to useContext hook

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,71 +1,62 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./main.css";
 import { FaFacebook, FaGithub } from "react-icons/fa";
 import { FiInstagram, FiMail } from "react-icons/fi";
 import DataContext from "../../context/datacontext";
 
 const Main = () => {
+  const value = useContext(DataContext);
   document.title = "Nirjal's Portfolio | Home";
   return (
-    <DataContext.Consumer>
-      {(value) => {
-        return (
-          <div className="main">
-            <div className="irregularShape1 irregularShape"></div>
-            <div className="irregularShape2 irregularShape"></div>
-            <div className="gridElement1">
-              <h1 className="greet">
-                {value.home.greet},
-                <span className="blueColored">{value.home.greetHighLight}</span>
-              </h1>
-              <div className="dots"></div>
-              <div className="dots"></div>
-              <div className="dots"></div>
-              <div className="dots"></div>
-              <p className="intro">{value.home.message}</p>
-              <br />
-              <p className="message">
-                <span className="identity">
-                  {value.home.IAM.map((identity) => (
-                    <span key="identity">{identity}</span>
-                  ))}
-                </span>
-              </p>
-              <br />
-              <div className="socials">
-                <p>
-                  <a href={value.social.facebook}>
-                    <FaFacebook /> Facebook
-                  </a>
-                </p>
-                <p>
-                  <a href={value.social.instagram}>
-                    <FiInstagram /> Instagram
-                  </a>
-                </p>
-                <p>
-                  <a href={value.social.github}>
-                    <FaGithub /> Github
-                  </a>
-                </p>
-                <p>
-                  <a href={value.social.gmail}>
-                    <FiMail /> Gmail
-                  </a>
-                </p>
-              </div>
-            </div>
-            <div className="gridElement2">
-              <img
-                src={value.home.avatar}
-                alt="profile"
-                className="profilePhoto"
-              />
-            </div>
-          </div>
-        );
-      }}
-    </DataContext.Consumer>
+    <div className="main">
+      <div className="irregularShape1 irregularShape"></div>
+      <div className="irregularShape2 irregularShape"></div>
+      <div className="gridElement1">
+        <h1 className="greet">
+          {value.home.greet},
+          <span className="blueColored">{value.home.greetHighLight}</span>
+        </h1>
+        <div className="dots"></div>
+        <div className="dots"></div>
+        <div className="dots"></div>
+        <div className="dots"></div>
+        <p className="intro">{value.home.message}</p>
+        <br />
+        <p className="message">
+          <span className="identity">
+            {value.home.IAM.map((identity) => (
+              <span key="identity">{identity}</span>
+            ))}
+          </span>
+        </p>
+        <br />
+        <div className="socials">
+          <p>
+            <a href={value.social.facebook}>
+              <FaFacebook /> Facebook
+            </a>
+          </p>
+          <p>
+            <a href={value.social.instagram}>
+              <FiInstagram /> Instagram
+            </a>
+          </p>
+          <p>
+            <a href={value.social.github}>
+              <FaGithub /> Github
+            </a>
+          </p>
+          <p>
+            <a href={value.social.gmail}>
+              <FiMail /> Gmail
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className="gridElement2">
+        <img src={value.home.avatar} alt="profile" className="profilePhoto" />
+      </div>
+    </div>
   );
 };
 
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./projects.css";
 import DataContext from "../../context/datacontext";
 import { GoGlobe } from "react-icons/go";
@@ -8,59 +8,56 @@ import { Link } from "react-router-dom";
 
 const Projects = () => {
   document.title = "Nirjal's Portfolio | Projects";
+  const data = useContext(DataContext);
   return (
     <div className="projectContainer">
       <div className="projects">
-        <DataContext.Consumer>
-          {(data) => {
-            return data.projects.map((project) => (
-              <div className="project">
-                <h3 className="projectName">{project.name}</h3>
-                <div className="techUsed">
-                  {project.techs.map((tech) => (
-                    <span className="techName" key={tech}>
-                      {tech}
-                    </span>
-                  ))}
-                </div>
-                <div className="dots"></div>
-                <div className="dots"></div>
-                <div className="dots"></div>
-                <div className="dots"></div>
+        {data.projects.map((project) => (
+          <div className="project">
+            <h3 className="projectName">{project.name}</h3>
+            <div className="techUsed">
+              {project.techs.map((tech) => (
+                <span className="techName" key={tech}>
+                  {tech}
+                </span>
+              ))}
+            </div>
+            <div className="dots"></div>
+            <div className="dots"></div>
+            <div className="dots"></div>
+            <div className="dots"></div>
 
-                <br />
-                <p className="projectDescription">{project.desp}</p>
-                <p className="projectLinks">
-                  {project.liveURL ? (
-                    <Link
-                      target="_blank"
-                      rel="noreferrer"
-                      to={{ pathname: project.liveURL }}
-                    >
-                      <GoGlobe className="web" />
-                      Live URL
-                    </Link>
-                  ) : (
-                    <p className="noURL">
-                      <GiCrossMark className="crossmark" /> No Github URL
-                    </p>
-                  )}
-                  <br />
-                  {project.githubURL && (
-                    <Link
-                      target="_blank"
-                      rel="noreferrer"
-                      to={{ pathname: project.githubURL }}
-                    >
-                      <AiFillGithub className="github" />
-                      Github URL
-                    </Link>
-                  )}
+            <br />
+            <p className="projectDescription">{project.desp}</p>
+            <p className="projectLinks">
+              {project.liveURL ? (
+                <Link
+                  target="_blank"
+                  rel="noreferrer"
+                  to={{ pathname: project.liveURL }}
+                >
+                  <GoGlobe className="web" />
+                  Live URL
+                </Link>
+              ) : (
+                <p className="noURL">
+                  <GiCrossMark className="crossmark" /> No Github URL
                 </p>
-              </div>
-            ));
-          }}
-        </DataContext.Consumer>
+              )}
+              <br />
+              {project.githubURL && (
+                <Link
+                  target="_blank"
+                  rel="noreferrer"
+                  to={{ pathname: project.githubURL }}
+                >
+                  <AiFillGithub className="github" />
+                  Github URL
+                </Link>
+              )}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
